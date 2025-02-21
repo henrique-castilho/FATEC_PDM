f4d92fd9aaf12c42ac946c95f5f9d1be7a5fd874
henrique-castilho <henrique.castilho.pires@gmail.com>
2025-02-21 19:42:36 -0300

    learn(Ex03-react): definir estilos do elemento 1 com função que retorna JSON

---
+++ exercicio_03/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  const containerStyles = {width: 1280, margin: 'auto', border: '1px solid black', backgroundColor: "#EEE",
+    borderRadius: 8, padding: 12, textAlign: 'center'};
+  return (
+    <div style={containerStyles}>
+      <h2>Profissionais de saúde</h2>
+    </div>
+  )
+}
+
+export default App
