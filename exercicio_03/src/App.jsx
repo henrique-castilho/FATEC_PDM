de1117480373e8a40f30fd230cedfd3157e974eb
henrique-castilho <henrique.castilho.pires@gmail.com>
2025-02-21 22:31:38 -0300

    learn(Ex03-react): cria nomes dos médicos e arquivo para estilização

--- exercicio_03/src/App.jsx
+++ exercicio_03/src/App.jsx
@@ -1,11 +1,22 @@
+import './styles.css'
 function App() {
   const containerStyles = {width: 1280, margin: 'auto', border: '1px solid black', backgroundColor: "#EEE",
     borderRadius: 8, padding: 12, textAlign: 'center'};
+  const docNames = {doc1: 'José da Silva', doc2: 'Maria da Silva', doc3: "Jaqueline Mendes"}
   return (
     <div style={containerStyles}>
       <h2>Profissionais de saúde</h2>
       <div style={{margin: 8, border: '1px solid #DDD', borderRadius: 8, padding: 8,
         display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
+        <div className="profissional">
+          <p>{docNames.doc1}</p>
+        </div>
+        <div className="profissional">
+          <p>{docNames.doc2}</p>
+        </div>
+        <div className='profissional'>
+          <p>{docNames.doc3}</p>
+        </div>
       </div>
     </div>
   )
