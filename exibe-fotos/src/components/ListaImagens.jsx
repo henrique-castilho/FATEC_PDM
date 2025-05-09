04f62a3794548df5e296dcf7b7d5d573401caec8
henrique-castilho <henrique.castilho.pires@gmail.com>
2025-05-09 15:33:24 -0300

    learn(app07-react: encerra busca com axios:

---
+++ exibe-fotos/src/components/ListaImagens.jsx
@@ -0,0 +1,11 @@
+//refce
+import React from 'react'
+import Imagem from './Imagem'
+
+const ListaImagens = ({photos}) => {
+  return (
+    photos.map((photo, key) => (<Imagem key={key} src={photo.src.small} alt={photo.alt}/>))
+  )
+}
+
+export default ListaImagens
---
+++ exibe-fotos/src/components/PexelsLogo.jsx
@@ -0,0 +1,15 @@
+//rafce
+import React from 'react'
+
+const PexelsLogo = () => {
+  return (
+    <a href="https://www.pexels.com">
+        <img 
+        width={75}
+        src="https://images.pexels.com/lib/api/pexels-white.png" 
+        alt="Logo da Pexels" />
+    </a>
+   )
+}
+
+export default PexelsLogo
