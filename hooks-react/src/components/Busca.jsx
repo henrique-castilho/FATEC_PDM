07e2407b71c1ff2b6d9a36d1d92f4eb8b1a03c67
henrique-castilho <henrique.castilho.pires@gmail.com>
2025-05-30 17:49:10 -0300

    learn(ap08-react): encerra a apostila
    -conteudo tambem finalizado

--- hooks-react/src/components/Busca.jsx
+++ hooks-react/src/components/Busca.jsx
@@ -1,6 +1,8 @@
 // rafce
-import React, { useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import striptags from 'striptags'
+import { Button } from 'primereact/button'
 import { IconField } from 'primereact/iconfield'
 import { InputIcon } from 'primereact/inputicon'
 import { InputText } from 'primereact/inputtext'
@@ -8,30 +10,67 @@ const Busca = () => {
     const [termoDeBusca, setTermoDeBusca] = useState('')
     const [resultados, setResultados] = useState([])
 
+    // useEffect(() => {
+    //     console.log('Causando algum efeito colatereal...')
+    //     return () => {
+    //         console.log("Fazendo uma limpeza qualquer, ou seja desalocando recursos")
+    //     }
+    // })
+
     useEffect(() => {
         const buscar = async () => {
             const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
-                    acction: 'query',
+                    action: 'query',
                     list: 'search',
                     format: 'json',
                     origin: '*',
                     srsearch: termoDeBusca
                 }
             })
-            setResultados(data)
+            setResultados(data.query.search)
         }
-        buscar()
+        const timeoutID = setTimeout(() => {
+            if (termoDeBusca)
+                buscar()
+        }, 1000)
+        return () => {
+            clearTimeout(timeoutID)
+        }
     }, [termoDeBusca])
 
     return (
-        <IconField iconPosition='left'>
-            <InputIcon className='pi pi-search' />
-            <InputText 
-                placeholder='Buscar...' 
-                onChange={(e) => {setTermoDeBusca(e.target.value)}}
-                value={termoDeBusca}/>
-        </IconField>
+        <div>
+            <IconField iconPosition='left'>
+                <InputIcon className='pi pi-search' />
+                <InputText
+                    placeholder='Buscar...'
+                    onChange={(e) => { setTermoDeBusca(e.target.value) }}
+                    value={termoDeBusca} />
+            </IconField>
+            {
+                resultados.map((resultado) => (
+                    <div
+                        key={resultado.pageid}
+                        className='my-3 border border-1 border-400'>
+                        <div
+                            className='border-bottom border border-1 border-400 p-3 text-center font-bold'>
+                            {resultado.title}
+                            <span>
+                                <Button
+                                    icon='pi pi-send'
+                                    className='ml-3 p-button-rounded p-button-secondary'
+                                    onClick={() => window.open(`https://en.wikipedia.org?currid=${resultado.pageid}`)} />
+                            </span>
+                        </div>
+                        <div
+                            className='p-1'>
+                            {striptags(resultado.snippet)}
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
     )
 }
 
